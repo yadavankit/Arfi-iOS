3f1abaf650558f563fe2e116a3d73ee324e1d650
Aditya Yadav <aditya.y@getsuperapp.com>
2016-06-17 14:49:21 +0530

    initial commit

---
+++ SnapchatCamera/nn.swift
@@ -0,0 +1,26 @@
+//
+//  nn.swift
+//  Udiva
+//
+//  Created by Aditya  Yadav on 16/06/16.
+//  Copyright © 2016 Archetapp. All rights reserved.
+//
+
+import UIKit
+
+class nn : UIView {
+    
+    
+    class func instanceFromNib() -> UIView {
+        return UINib(nibName: "nn", bundle: nil).instantiateWithOwner(nil, options: nil)[0] as! UIView
+    }
+    
+    
+
+    
+    
+    
+    
+    
+    
+}
---
+++ Udiva-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Udiva-Bridging-Header.h
+//  Udiva
+//
+//  Created by Aditya  Yadav on 16/06/16.
+//  Copyright © 2016 Archetapp. All rights reserved.
+//
+
+#ifndef Udiva_Bridging_Header_h
+#define Udiva_Bridging_Header_h
+
+#import "Cloudinary.h"
+
+
+#endif /* Udiva_Bridging_Header_h */
